fa4ca5f6b93a27a11c92d341518b97415d448f64
Bozzi Marcello <bozzimarcello@gmail.com>
2021-02-10 16:06:07 +0100

    prima commit

---
+++ AppXamarin/AppXamarin/AppXamarin/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using AppXamarin.ViewModels;
+using AppXamarin.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AppXamarin
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
